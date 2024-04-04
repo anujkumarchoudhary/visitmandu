80ef89bada51c8215dcacf0f035e12cde4ff79f2
anujkumarchoudhary <timetechapps1@gmail.com>
2024-04-04 10:32:52 -0700

    changes by anuj

---
+++ src/pages/about/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './About.module.scss'
+import { Grid, Typography } from '@mui/material'
+
+const About = () => {
+    return (
+        <Grid className={styles.aboutContainer}>
+            <Typography>Hello</Typography>
+        </Grid>
+    )
+}
+
+export default About
